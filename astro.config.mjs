e280494529038f07b68cc8ccc2f11b737ada1962
Peter Desmet <peter.desmet@inbo.be>
2025-01-07 11:34:00 +0100

    Add remarkHeading settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import { remarkHeadingId } from 'remark-custom-heading-id';
 
 // https://astro.build/config
 export default defineConfig({
@@ -26,4 +27,7 @@ export default defineConfig({
       ],
     }),
   ],
+  markdown: {
+    remarkPlugins: [remarkHeadingId]
+  }
 });
