8fe948ccd5060b28bd199a4908cf92b8e8ae85cc
Sanne Govaert <44606923+sannegovaert@users.noreply.github.com>
2025-03-10 09:34:03 +0100

    Update file paths

--- gcube_rmd_to_md.R
+++ gcube_rmd_to_md.R
@@ -1,3 +1,5 @@
+devtools::install_github("b-cubed-eu/b3doc#1")
+
 library(b3doc)
 library(purrr)
 
@@ -9,8 +11,8 @@ rmd_files <- c(
   "https://raw.githubusercontent.com/b-cubed-eu/gcube/refs/heads/main/vignettes/articles/multi-species-approach.Rmd"
 )
 
-md_dir <- file.path("output", "src", "content", "docs", "r", "gcube")
-fig_dir <- file.path("output", "public", "r", "gcube")
-fig_url_dir <- "/astro-docs/r/gcube/"
+md_dir <- file.path("src", "content", "docs", "software", "gcube")
+fig_dir <- file.path("public", "software", "gcube")
+fig_url_dir <- "/software/gcube/"
 
 purrr::walk(rmd_files, ~rmd_to_md(.x, md_dir, fig_dir, fig_url_dir))
